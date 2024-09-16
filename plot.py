f7c83172bce4792ff48a708bfa3065ce71d750c4
Eric Ogden <cf22202@bristol.ac.uk>
2024-09-16 15:20:42 +0100

    Upload plot onto git

---
+++ plot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,0,4],[1,0,-1])
+plt.show()
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,0,4],[1,0,-1])
+plt.show()
+
+
